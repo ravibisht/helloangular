b4fe9ed0b565d6d5939605267342a79c6a4113a7
Ravi Bisht <47188858+ravibisht@users.noreply.github.com>
2023-05-05 23:38:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1> "Your CTO Samarth  Sharma "</h1>`,
+  template: `<h1> "Your CTO Samarth  Sharma Sir,"</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
